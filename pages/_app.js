0c3cdd84c19464d84a86fcf5d5394bfff92a3133
carlos <carloshrabelo@hotmail.com>
2020-09-19 11:49:03 -0300

    fix: fixing import into _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Layout from "Layout";
+import Layout from "layout";
 import Provider from "store";
 
 const App = ({ Component, pageProps }) => (
